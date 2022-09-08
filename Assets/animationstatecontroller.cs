9693bc3168cdab82a4a1e71ad5d0ad0ff19c5ab9
Ege Can Sarı <89903168+E9ex@users.noreply.github.com>
2022-09-08 11:48:14 +0300

    abiyaptım

--- Assets/animationstatecontroller.cs
+++ Assets/animationstatecontroller.cs
@@ -19,26 +19,29 @@ public class animationstatecontroller : MonoBehaviour
         bool isrunning = animator.GetBool("isrunning");
         bool iswalking = animator.GetBool(iswalkinghash);
         bool forwardpressed = Input.GetKey("w");
-        bool runpress = Input.GetKey("z");
-        if (!iswalking&&forwardpressed) 
+        bool runpress = Input.GetKey("left shift");
+        
+        
+        
+        if (!iswalking && forwardpressed) 
             
         {
             animator.SetBool(iswalkinghash,true);
         }
 
-        if (iswalking&&!forwardpressed)
+        if (iswalking && !forwardpressed)
         {
             animator.SetBool(iswalkinghash,false);
         }
-        if (!isrunning&&(forwardpressed&&runpress) )
+        if (!isrunning && (forwardpressed&&runpress) )
             
         {
             animator.SetBool("isrunning",true);
         }
 
-        if (!iswalking&&(forwardpressed&& runpress))
+        if (isrunning&&(!forwardpressed|| !runpress))
         {
-            animator.SetBool("isrunning",true);
+            animator.SetBool("isrunning",false);
         }
         
     }
